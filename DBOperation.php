fb4dc9689da8ebfbbad29f8c179491e5cff31139
Felipe Castillo <felipe.castillo.barraza@cftmail.cl>
2024-07-02 21:18:40 -0400

    sensor de gases

---
+++ DBOperation.php
@@ -0,0 +1,99 @@
+<?php
+class DBOperation
+{
+    private $con;
+
+    // Constructor de donde sacamos las credenciales de la Base de Datos del archivo conexion.php
+    function __construct()
+    {
+        require_once dirname(__FILE__) . '/conexion.php';
+        $db = new DBconnect();
+        $this->con = $db->connect();
+    }
+
+    // Funcion que nos permite insertar un usuario, en este caso no la ocuparemos
+    public function insertUser($idtipo_usuario, $nombre, $email, $password, $apiKey)
+    {
+        $stmt = $this->con->prepare("INSERT INTO usuarios(idtipo_usuario, nombre, email, Password, apiKey) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssss", $idtipo_usuario, $nombre, $email, $password, $apiKey);
+        if ($stmt->execute())
+            return true;
+        return false;
+    }
+
+    // Funcion que nos permite realizar el login
+    public function loginUser($email, $password) {
+        // Toma dos parametros $password y $email, tambien realiza una consulta y se utiliza 'prepare' para evitar inyecciones SQL
+        $stmt = $this->con->prepare("SELECT password FROM usuario WHERE email = ?");
+        // verifica si la consulta fue exitosa
+        if ($stmt) {
+            $stmt->bind_param("s", $email); // vincula el parametro email a la consulta y le indica que es un string "s"
+            $stmt->execute(); // ejecuta la consulta
+            $stmt->store_result(); // almacena el resultado en el objeto $stmt
+            if ($stmt->num_rows > 0) { // verifica si le llego algun resultado
+                $stmt->bind_result($storedPassword); // vincula $storedPassword al resultado de la consulta, para almacenar la contraseña recuperada
+                $stmt->fetch(); // obtiene el resultado y lo almacena en la variable
+                // Verificar si la contraseña proporcionada coincide con la contraseña almacenada
+                if ($password === $storedPassword) { // si coincide esta todo correcto
+                    $response['error'] = false;
+                    $response['message'] = 'Login successful';
+                } else { // si falla, quiere decir que la contraseña esta mala
+                    $response['error'] = true;
+                    $response['message'] = 'Invalid password';
+                }
+            } else { // tambien puede fallar el correo
+                $response['error'] = true;
+                $response['message'] = 'Email not found';
+            }
+        } else {
+            $response['error'] = true;
+            $response['message'] = 'Error preparing statement';
+        }
+        $stmt->close(); // cierra la consulta
+        return $response; // devuelve la respuesta
+    }
+    
+    // Funcion que nos permite obtener los usuarios registrados
+    public function getUser()
+    {
+        $stmt = $this->con->prepare("SELECT idusuario, idtipo_usuario, nombre, email FROM usuario");
+        $stmt->execute();
+        $stmt->bind_result($idusuario, $idtipo_usuario, $nombre, $email);
+        $usuarios = array();
+
+        while ($stmt->fetch()) {
+            $temp = array();
+            $temp['idusuario'] = $idusuario;
+            $temp['idtipo_usuario'] = $idtipo_usuario;
+            $temp['nombre'] = $nombre;
+            $temp['email'] = $email;
+            array_push($usuarios, $temp);
+        }
+        return $usuarios;
+    }
+
+    // Funcion que nos permite obtener los registros que debemos obtener del servidor
+    public function getRegister()
+    {
+        // Prepara una consulta SQL donde obtendra las columnas idregistro, idestado_registro, idtipo_registro, registro y fecha de la tabla registro
+        $stmt = $this->con->prepare("SELECT id, idCaja, sensor1, sensor2, sensor3, sensor4, sensor5, fechaHora FROM registro");
+        $stmt->execute(); // ejecuta la consulta
+        $stmt->bind_result($id, $idCaja, $sensor1, $sensor2, $sensor3, $sensor4, $sensor5, $fechaHora); // vincula las variables con los resultados de la consulta
+        $registros = array(); // inicializa un arreglo vacio
+
+        while ($stmt->fetch()) { // asigna los valores de las variables vinculadas al arreglo $temp
+            $temp = array();
+            $temp['id'] = $id;
+            $temp['idCaja'] = $idCaja;
+            $temp['sensor1'] = $sensor1;
+            $temp['sensor2'] = $sensor2;
+            $temp['sensor3'] = $sensor3;
+            $temp['sensor4'] = $sensor4;
+            $temp['sensor5'] = $sensor5;
+            $temp['fecha'] = $fechaHora;
+            array_push($registros, $temp); // añade el arreglo temporal $temp al arreglo $registros
+        }
+        return $registros; // retorna el arreglo $registros
+    }
+}
+?>
---
+++ login_register_app.sql
@@ -0,0 +1,185 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-07-2024 a las 03:16:53
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `login_register_app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado`
+--
+
+CREATE TABLE `estado` (
+  `idestado` int(11) NOT NULL,
+  `estado` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `estado`
+--
+
+INSERT INTO `estado` (`idestado`, `estado`) VALUES
+(0, 'Inactivo'),
+(1, 'Activo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro`
+--
+
+CREATE TABLE `registro` (
+  `id` int(11) NOT NULL,
+  `idCaja` int(11) NOT NULL,
+  `sensor1` int(11) NOT NULL,
+  `sensor2` int(11) NOT NULL,
+  `sensor3` int(11) NOT NULL,
+  `sensor4` int(11) NOT NULL,
+  `sensor5` int(11) NOT NULL,
+  `fechaHora` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `registro`
+--
+
+INSERT INTO `registro` (`id`, `idCaja`, `sensor1`, `sensor2`, `sensor3`, `sensor4`, `sensor5`, `fechaHora`) VALUES
+(1, 1, 10, 10, 10, 10, 10, '2024-07-03 02:41:18'),
+(2, 1, 10, 10, 10, 10, 10, '2024-07-03 02:41:18'),
+(3, 2, 100, 100, 100, 100, 100, '2024-07-03 02:59:12'),
+(4, 1, 100, 100, 100, 100, 400, '2024-07-03 03:01:19'),
+(5, 2, 1, 1, 1, 1, 400, '2024-07-03 03:13:46'),
+(6, 2, 1, 1, 1, 1, 600, '2024-07-03 03:14:11');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo`
+--
+
+CREATE TABLE `tipo` (
+  `idtipo` int(11) NOT NULL,
+  `tipo` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipo`
+--
+
+INSERT INTO `tipo` (`idtipo`, `tipo`) VALUES
+(1, 'Admin'),
+(2, 'Guest'),
+(500, 'consulta');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `idusuario` int(11) NOT NULL,
+  `idtipo_usuario` int(11) NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `apiKey` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`idusuario`, `idtipo_usuario`, `nombre`, `email`, `password`, `apiKey`) VALUES
+(1, 1, 'Test', 'test@test.com', '2908', '123');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `estado`
+--
+ALTER TABLE `estado`
+  ADD PRIMARY KEY (`idestado`);
+
+--
+-- Indices de la tabla `registro`
+--
+ALTER TABLE `registro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipo`
+--
+ALTER TABLE `tipo`
+  ADD PRIMARY KEY (`idtipo`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idusuario`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`),
+  ADD KEY `fk_usuario_tipo_idx` (`idtipo_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `estado`
+--
+ALTER TABLE `estado`
+  MODIFY `idestado` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `registro`
+--
+ALTER TABLE `registro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo`
+--
+ALTER TABLE `tipo`
+  MODIFY `idtipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=501;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD CONSTRAINT `fk_usuario_tipo` FOREIGN KEY (`idtipo_usuario`) REFERENCES `tipo` (`idtipo`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
